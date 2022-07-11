c1ce41bf013a5b1c3f169642f17257c308bd6d71
Chadd <chadd_artaud@cox.net>
2022-07-11 11:04:11 -0700

    fixed mistake in filtering fetch call and progress on checkout

--- Develop/controllers/api/searchRoutes.js
+++ Develop/controllers/api/searchRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Artist, Album, Song } = require('../../models');
+const { Artist, Album, Song, Playlist } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/:genre', withAuth, async (req, res) => {
@@ -30,5 +30,20 @@ router.get('/:genre', withAuth, async (req, res) => {
         res.status(500).json(err)
     }
 });
+
+router.get('/checkout', async (req, res) => {
+    try {
+        // const songs = req.body
+        // for (let i =0, i< songs.length; i++) {
+
+        // }
+        const request = await Playlist.create({song_id: 1, user_id: req.session.user_id})
+        
+        res.status(200).json(request)
+    } catch (err) {
+        console.log(req.body)
+      res.status(500).json(err)
+    }
+  })
 
 module.exports = router;
